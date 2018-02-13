07478eacfda7ad736503816fa7f729a1512f98e6
axc2629 <axc2629@zag-rm11-1012.main.ad.rit.edu>
2018-02-13 14:54:53 +0100

    Project construction

---
+++ Photography/public_html/js/presentation/newjavascript.js
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+//objecty od business layera i zvati 
+//comunicitrra sa business layerom
+//a user comunicira presentation
+
+
